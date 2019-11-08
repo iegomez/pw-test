ed4669633aea9c89f096ea601e1f0c8f00b42a0e
Ignacio Gómez <iegomez@manglar.cl>
2019-11-08 16:57:37 -0300

    Push the tool.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iegomez/pw-test
+
+go 1.13
+
+require github.com/iegomez/mosquitto-go-auth v0.0.0-20191106133151-cbed8afc08db
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/iegomez/mosquitto-go-auth/common"
+)
+
+func main() {
+
+	var password = flag.String("p", "", "plain text password")
+	var hashed = flag.String("h", "", "pbkdf2 password hash. use single quotes so $ symbols aren't taken as variables")
+
+	flag.Parse()
+
+	if common.HashCompare(*password, *hashed) {
+		fmt.Println("success: plain and hashed passwords match")
+		return
+	}
+	fmt.Println("error: plain and hashed passwords don't match")
+	os.Exit(1)
+}
